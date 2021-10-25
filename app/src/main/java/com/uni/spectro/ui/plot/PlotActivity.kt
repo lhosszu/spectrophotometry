6eee2312fee395c7a2c740011c270819effd6610
lhosszu <levihosszu@gmail.com>
2021-10-25 20:10:18 +0200

    Refactor Plot activity

--- app/src/main/java/com/uni/spectro/ui/plot/PlotActivity.kt
+++ app/src/main/java/com/uni/spectro/ui/plot/PlotActivity.kt
@@ -1,6 +1,5 @@
 package com.uni.spectro.ui.plot
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -22,66 +21,86 @@ import java.text.ParsePosition
 
 class PlotActivity : AppCompatActivity() {
 
-    private val sensorCalibration: SensorCalibration = SensorCalibration()
+    private lateinit var plot: XYPlot
+    private lateinit var maxPointInfo: TextView
+    private lateinit var experimentName: TextView
 
-    @SuppressLint("DefaultLocale")
     public override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_plot)
 
-        val plot = findViewById<XYPlot>(R.id.plot)
-        val maxPointInfo = findViewById<TextView>(R.id.text_maximum_info)
+        initFields()
+
         val experiment = fromIntent
+        experimentName.text = experiment.name
+        if (experiment.selectedWavelength != 0) {
+            maxPointInfo.text = maxPointInfo(experiment)
+        }
 
-        try {
-            findViewById<TextView>(R.id.text_experiment_name_on_plot).text = experiment.name
+        plot(experiment)
+    }
 
-            if (experiment.selectedWavelength != 0) {
-                maxPointInfo.text = String.format("max wavelength: %d\nmax %s: %.4f", experiment.selectedWavelength, experiment.experimentType!!.toLowerCase(), experiment.intensity)
+    private fun plot(experiment: ExperimentWrapper) {
+        setPlotProperties()
+        when (experiment.points.size) {
+            SpectrumConstants.VISIBLE_RANGE_SIZE -> {
+                val series: XYSeries = SimpleXYSeries(SensorCalibration().visibleRange(), experiment.points, "")
+                plot.addSeries(series, plotFormat())
             }
 
-            val data = experiment.points
-            var series: XYSeries? = null
+            SpectrumConstants.PIXEL_COUNT -> {
+                val series: XYSeries = SimpleXYSeries(SensorCalibration().fullRange(), experiment.points, "")
+                plot.addSeries(series, plotFormat())
+            }
 
-            if (experiment.points!!.size == SpectrumConstants.VISIBLE_RANGE_SIZE) {
-                series = SimpleXYSeries(sensorCalibration.visibleRange(), data, "")
-            } else if (data!!.size == SpectrumConstants.PIXEL_COUNT) {
-                series = SimpleXYSeries(sensorCalibration.fullRange(), data, "")
-            } else {
-                Log.d(TAG, "Cannot plot NULL series")
+            else -> {
+                Log.e(TAG, "Cannot plot NULL series")
                 finish()
             }
+        }
+    }
 
-            val lightGreen = Color.rgb(218, 227, 229)
-            val format = LineAndPointFormatter(lightGreen, null, lightGreen, null)
-            format.interpolationParams = CatmullRomInterpolator.Params(70, CatmullRomInterpolator.Type.Centripetal)
-            format.isLegendIconEnabled = false
+    private fun initFields() {
+        plot = findViewById(R.id.plot)
+        maxPointInfo = findViewById(R.id.text_maximum_info)
+        experimentName = findViewById(R.id.text_experiment_name_on_plot)
+    }
 
-            plot.graph.domainGridLinePaint.color = Color.WHITE
-            plot.graph.rangeGridLinePaint.color = Color.WHITE
+    private fun setPlotProperties() {
+        plot.graph.domainGridLinePaint.color = Color.WHITE
+        plot.graph.rangeGridLinePaint.color = Color.WHITE
+        plot.graph.getLineLabelStyle(XYGraphWidget.Edge.LEFT).format = lineLabelFormat()
+        plot.setDomainLabel("wavelength [nm]")
+        plot.setRangeLabel("intensity [-]")
+        plot.domainStepValue = 8.0
+        plot.rangeStepValue = 0.0
+        plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = DecimalFormat("#")
+        plot.domainTitle.position(0f, HorizontalPositioning.ABSOLUTE_FROM_CENTER, 50f, VerticalPositioning.ABSOLUTE_FROM_BOTTOM, Anchor.BOTTOM_MIDDLE)
+        plot.rangeTitle.position(50f, HorizontalPositioning.ABSOLUTE_FROM_LEFT, 0f, VerticalPositioning.RELATIVE_TO_CENTER, Anchor.LEFT_MIDDLE)
+        plot.legend.isVisible = false
+    }
 
-            plot.graph.getLineLabelStyle(XYGraphWidget.Edge.LEFT).format = object : Format() {
-                override fun format(obj: Any, toAppendTo: StringBuffer, pos: FieldPosition): StringBuffer {
-                    return StringBuffer("")
-                }
+    private fun plotFormat(): LineAndPointFormatter {
+        val format = LineAndPointFormatter(spectrumColor(), null, spectrumColor(), null)
+        format.interpolationParams = CatmullRomInterpolator.Params(70, CatmullRomInterpolator.Type.Centripetal)
+        format.isLegendIconEnabled = false
+        return format
+    }
 
-                override fun parseObject(source: String, pos: ParsePosition): Any? {
-                    return null
-                }
-            }
+    private fun spectrumColor(): Int {
+        return Color.rgb(218, 227, 229)
+    }
 
-            plot.setDomainLabel("wavelength [nm]")
-            plot.setRangeLabel("intensity [-]")
-            plot.domainStepValue = 8.0
-            plot.rangeStepValue = 0.0
-            plot.graph.getLineLabelStyle(XYGraphWidget.Edge.BOTTOM).format = DecimalFormat("#")
-            plot.domainTitle.position(0f, HorizontalPositioning.ABSOLUTE_FROM_CENTER, 50f, VerticalPositioning.ABSOLUTE_FROM_BOTTOM, Anchor.BOTTOM_MIDDLE)
-            plot.rangeTitle.position(50f, HorizontalPositioning.ABSOLUTE_FROM_LEFT, 0f, VerticalPositioning.RELATIVE_TO_CENTER, Anchor.LEFT_MIDDLE)
-            plot.addSeries(series, format)
-            plot.legend.isVisible = false
-        } catch (e: Exception) {
-            e.printStackTrace()
-            super.finish()
+    private fun maxPointInfo(experiment: ExperimentWrapper) =
+            String.format("max wavelength: %d\nmax %s: %.4f", experiment.selectedWavelength, experiment.experimentType.toLowerCase(), experiment.intensity)
+
+    private fun lineLabelFormat() = object : Format() {
+        override fun format(obj: Any, toAppendTo: StringBuffer, pos: FieldPosition): StringBuffer {
+            return StringBuffer("")
+        }
+
+        override fun parseObject(source: String, pos: ParsePosition): Any? {
+            return null
         }
     }
 
@@ -91,7 +110,7 @@ class PlotActivity : AppCompatActivity() {
             return JsonConverter().toExperiment(experimentJson)
         }
 
-    companion object {
+    private companion object {
         private val TAG = PlotActivity::class.java.name
     }
 }
